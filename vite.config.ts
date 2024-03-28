c24901f29a8a923174bee6c41383963ecfffa0a6
Hossein79 <hosseinfouladi20@gmail.com>
2024-03-28 14:15:45 +0330

    test4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig ,loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+
+const env=loadEnv('production','')
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,8 +15,9 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL(process.env.NODE_ENV==='production'?'./portfolio/src':'./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  base:env.VITE_ENV==='production'?'/portfolio/':'/'
   
 })
